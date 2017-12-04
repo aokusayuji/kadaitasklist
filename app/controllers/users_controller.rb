b22e1c903163b02247953071ffd8ea64d50141d7
yujiaokusa <aokusayuji@gmail.com>
2017-12-04 23:44:27 +0000

    post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:show, :edit, :update, :destroy]
   
   def index
-    @users = User.all.page(params[:page])
+    @users = User.page(params[:page])
   end
 
   def show
